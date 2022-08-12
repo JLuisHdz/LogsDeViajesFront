40ffba17c15ed54e8ae976b7512062e302a7ad62
Luis Hdz <joseluishdz02@gmail.com>
2022-08-12 12:17:53 -0500

    feat: update the app and index

--- src/App.js
+++ src/App.js
@@ -1,39 +1,24 @@
-import './App.css';
+import NavBar from "./components/navbar/NavBar";
+import Login from "./pages/Login/login";
+import Register from "./pages/Register/register";
+import Crear from "./pages/Crear/crear";
+import Home from "./pages/home/Home";
+import { BrowserRouter as Router, Route, Routes, BrowserRouter } from "react-router-dom";
+import { useContext } from "react";
+import { Context } from "./context/Context";
 
 function App() {
+  const { user } = useContext(Context);
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-      <div className='form'>
-        <div>
-          <p>CREAR LOG</p>
-        </div>
-        <div className='form-control'>
-          <label>Usuario</label>
-          <input type="text" placeholder='nombre'/>
-        </div>
-        <div className='form-control'>
-          <label>Titulo</label>
-          <input type="text" placeholder='titulo'/>
-        </div>
-        <div className='form-control'>
-          <label>Descripcion</label>
-          <input type="text" placeholder='descripcion'/>
-        </div>
-        <div className='form-control'>
-          <label>ubicacion</label>
-          <input type="text" placeholder='ubicacion'/>
-        </div>
-        <div className='form-control'>
-          <label>fecha</label>
-          <input type="text" placeholder='fecha'/>
-        </div>
-        <div className='btn'>
-          <button className='btna'>Agregar</button>
-        </div>
-      </div>
-    </div>
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route exact path="/" element={<Home />}></Route>
+        <Route path="/register" element={user ? <Home /> : <Register />}></Route>
+        <Route path="/login" element={user ? <Home /> : <Login />}></Route>
+        <Route path="/crear" element={user ? <Home /> : <Crear />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/listado.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function listado(){
-    return(
-        <div className="logs">
-          <p>pagina de listados</p>
-        </div>
-    )
-}
